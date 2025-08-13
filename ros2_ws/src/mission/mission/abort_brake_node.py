4fae934f6a71eab45c3205841a0a92427a515c6d
LaurentDuch <ducharmelaurent@gmail.com>
2025-08-13 00:50:39 -0400

    setting Mavros and Zenmav on different ports - stronger abort_brake node

--- ros2_ws/src/mission/mission/abort_brake_node.py
+++ ros2_ws/src/mission/mission/abort_brake_node.py
@@ -25,7 +25,7 @@ class AbortBrake(Node):
         self.subscriber_ab = self.create_subscription(String, '/abort_brake', self.abort_brake_callback, qos_profile)
         self.publisher_ab_call = self.create_publisher(String, '/close', qos_profile)
         self.position_sub = self.create_subscription(PoseStamped, '/mavros/local_position/pose', self.pose_callback, qos_profile_BE)
-        self.nav = Zenmav(ip = 'tcp:127.0.0.1:5762') # pour simu, parfois 5763 est dispo, mais pour brake, l'impression que 5762 est mieux
+        self.nav = Zenmav(ip = 'tcp:127.0.0.1:5762') # pour simu : 5762 pour Zenmav et 5763 pour Mavros
         #self.nav = Zenmav(ip = 'udp:127.0.0.1:14550') # pour test de vol
 
     def pose_callback(self,msg):
@@ -47,7 +47,10 @@ class AbortBrake(Node):
             self.nav.set_mode('GUIDED')
             self.get_logger().info(f'Guided mode successfully enforced at {time.time():.3f}! at ({self.curr_pos.x:.3f}, {self.curr_pos.y:.3f}, {self.curr_pos.z:.3f})')
 
-            AbortBrake().destroy_node()
+            time.sleep(1)
+
+            self.destroy_node()
+            time.sleep(1)
             rclpy.shutdown()
 
         else:
