57ee6dda7a9a2216c07bc368623faded4f45b0b5
Nicman1 <nic.champagne@outlook.com>
2025-09-21 22:23:28 -0400

    might have cooked

--- ros2_ws/src/mission/mission/web_manual_control_node.py
+++ ros2_ws/src/mission/mission/web_manual_control_node.py
@@ -38,14 +38,14 @@ class DroneWebControl(Node):
         self.sensor_data = {}
         
         self.control_publishers['/approach_target_graph'] = self.create_publisher(String, '/approach_target_graph', qos_profile)
-        self.control_publishers['/go_winch'] = self.create_publisher(String, '/go_winch', 10)
-        self.control_publishers['/bucket_number'] = self.create_publisher(Int32, '/bucket_number', 10)
-        self.control_publishers['/go_vision'] = self.create_publisher(String, '/go_vision', 10)
-        self.control_publishers['/abort_brake'] = self.create_publisher(String, '/abort_brake', qos_profile)
         self.control_publishers['/battery_changed'] = self.create_publisher(String, '/battery_changed', 10)
-        self.control_publishers['/confirm_arming'] = self.create_publisher(String, '/confirm_arming', 10)
-        self.control_publishers['/valve_state'] = self.create_publisher(String, '/valve_state', 10)
         self.control_publishers['/go_bucket_valve'] = self.create_publisher(String, '/go_bucket_valve', 10)
+        self.control_publishers['/go_vision'] = self.create_publisher(String, '/go_vision', 10)
+        self.control_publishers['/valve_state'] = self.create_publisher(String, '/valve_state', 10)
+        self.control_publishers['/bucket_number'] = self.create_publisher(Int32, '/bucket_number', 10)
+        self.control_publishers['/go_winch'] = self.create_publisher(String, '/go_winch', 10)
+        self.control_publishers['/confirm_arming'] = self.create_publisher(String, '/confirm_arming', 10)
+        self.control_publishers['/abort_brake'] = self.create_publisher(String, '/abort_brake', qos_profile)
         
         self.control_subscribers['/torque'] = self.create_subscription(Float32, '/torque', self.torque_callback, 10)
         self.sensor_data['/torque'] = 0
@@ -160,10 +160,10 @@ class DroneWebControl(Node):
                 command = message['command']
                 data = message.get('data')
                 
-                if command in self.control_publishers:  # Changed to use control_publishers directly
+                if command in self.control_publishers:
                     msg = String() if not command.endswith('bucket_number') else Int32()
-                    msg.data = data if data else command.split('/')[-1].upper()
-                    self.control_publishers[command.split('/')[-1]].publish(msg)
+                    msg.data = data if data else command
+                    self.control_publishers[command].publish(msg)
                     await self.send_terminal_message(f"Command executed: {command}")
                 else:
                     await self.send_terminal_message(f"Unknown command: {command}")
