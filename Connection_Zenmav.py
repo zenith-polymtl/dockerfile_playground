787809a733048092fdcf04c5ec80220815de5317
annabellecyr <annabelle.cyr@hotmail.fr>
2025-11-13 16:22:23 -0500

    rpi5 integration

--- Connection_Zenmav.py
+++ Connection_Zenmav.py
@@ -2,8 +2,8 @@ from zenmav.core import Zenmav
 import time
 
 #drone = Zenmav(ip = 'udp:127.0.0.1:14550', baud=115200, GCS = False, tcp_ports=[14551])
-drone = Zenmav()
+#drone = Zenmav()
 
-#drone = Zenmav(ip = '/dev/serial0', baud=115200)
+drone = Zenmav(ip = '/dev/serial0', baud=115200)
 
 #drone = Zenmav('udp:192.168.144.12:19856', baud=115200)
--- ros2_ws/src/drone_interfaces/drone_interfaces/DroneControlInterface.py
+++ ros2_ws/src/drone_interfaces/drone_interfaces/DroneControlInterface.py
@@ -37,20 +37,20 @@ class DroneControlInterface(Node):
         self.control_subscribers = {}
         self.sensor_data = {}
         
-        self.control_publishers['/go_bucket_valve'] = self.create_publisher(String, '/go_bucket_valve', 10)
         self.control_publishers['/abort_brake'] = self.create_publisher(String, '/abort_brake', qos_profile)
-        self.control_publishers['/valve_state'] = self.create_publisher(String, '/valve_state', 10)
-        self.control_publishers['/approach_target_graph'] = self.create_publisher(String, '/approach_target_graph', qos_profile)
-        self.control_publishers['/confirm_arming'] = self.create_publisher(String, '/confirm_arming', 10)
-        self.control_publishers['/battery_changed'] = self.create_publisher(String, '/battery_changed', 10)
-        self.control_publishers['/go_winch'] = self.create_publisher(String, '/go_winch', 10)
-        self.control_publishers['/bucket_number'] = self.create_publisher(Int32, '/bucket_number', 10)
         self.control_publishers['/go_vision'] = self.create_publisher(String, '/go_vision', 10)
+        self.control_publishers['/valve_state'] = self.create_publisher(String, '/valve_state', 10)
+        self.control_publishers['/battery_changed'] = self.create_publisher(String, '/battery_changed', 10)
+        self.control_publishers['/bucket_number'] = self.create_publisher(Int32, '/bucket_number', 10)
+        self.control_publishers['/confirm_arming'] = self.create_publisher(String, '/confirm_arming', 10)
+        self.control_publishers['/go_winch'] = self.create_publisher(String, '/go_winch', 10)
+        self.control_publishers['/approach_target_graph'] = self.create_publisher(String, '/approach_target_graph', qos_profile)
+        self.control_publishers['/go_bucket_valve'] = self.create_publisher(String, '/go_bucket_valve', 10)
         
-        self.control_subscribers['/water_qty'] = self.create_subscription(Int32, '/water_qty', self.water_qty_callback, 10)
-        self.sensor_data['/water_qty'] = 0
         self.control_subscribers['/torque'] = self.create_subscription(Float32, '/torque', self.torque_callback, 10)
         self.sensor_data['/torque'] = 0
+        self.control_subscribers['/water_qty'] = self.create_subscription(Int32, '/water_qty', self.water_qty_callback, 10)
+        self.sensor_data['/water_qty'] = 0
         
         # Web interface components
         self.active_connections: Set[WebSocket] = set()
@@ -80,12 +80,12 @@ class DroneControlInterface(Node):
         # Start webview
         self.start_webview()
 
-    def water_qty_callback(self, msg):
-        self.sensor_data['/water_qty'] = msg.data
-        self.schedule_async_task(self.broadcast_sensor_data())
-
     def torque_callback(self, msg):
         self.sensor_data['/torque'] = msg.data
+        self.schedule_async_task(self.broadcast_sensor_data())
+
+    def water_qty_callback(self, msg):
+        self.sensor_data['/water_qty'] = msg.data
         self.schedule_async_task(self.broadcast_sensor_data())
 
     def schedule_async_task(self, coro):
